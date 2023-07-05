282d228083e5d51502456b3b3845d03971934446
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2023-07-05 14:54:47 -0300

    feat:next auth provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,18 @@
+import { NextAuthProvider } from '@/providers/auth'
 import './globals.css'
 
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({
+  subsets: ['latin'], weight: [
+    '400',
+    '500',
+    '600',
+    '700',
+    '800',
+    '900',
+  ]
+})
 export const metadata = {
   title: 'Trips',
   description: 'Book your trip',
@@ -12,7 +25,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={poppins.className}>
+        <NextAuthProvider>
+          {children}
+        </NextAuthProvider>
+
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+'use client'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 export default function Home() {
+  const { data } = useSession()
   return (
-    <h1>home page</h1>
+    <div>
+      <button onClick={() => signIn()}>Login</button>
+      <button onClick={() => signOut()}>Logout</button>
+      <h1 className="text-3xl font-bold text-red-500 underline ">
+        ola {data?.user?.name}
+        <img src={data?.user?.image} alt="" />
+      </h1>
+    </div>
+
+
+
   )
 }
